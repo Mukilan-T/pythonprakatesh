192af986c64e6cc2a4fcf5de7527be69d86543db
prakatesh <prakatesh2020@gmail.com>
2021-11-29 18:29:42 +0530

    merge conflit

--- new.py
+++ new.py
@@ -8,3 +8,4 @@ fahrenheit = (celsius * 1.8) + 32
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
 print("I'm Mukilan")
 print("I am prakatesh")
+print("Final_demo)
